fbb4839315babaeff2c53c8f38346eff9ce14030
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2024-11-15 16:00:02 +0530

    Committing MultiThreading Code Changes

---
+++ 19.MultiThreading/src/threads/ThreadExampleRunnable.java
@@ -0,0 +1,37 @@
+package threads;
+
+
+
+class MyRunnable implements Runnable
+{
+    public void run()
+    {
+        int i=1;
+        while(true)
+        {
+            System.out.println(i+"Hello");
+            i++;
+        }
+    }
+}
+public class ThreadExampleRunnable
+{
+    
+    
+    public static void main(String[] args) {
+        
+        MyRunnable t=new MyRunnable();
+        Thread th=new Thread(t);
+        
+        th.start();
+        
+        int i=1;
+        while(true)
+        {
+            System.out.println(i+"World");
+            i++;
+        }
+        
+    }
+    
+}
---
+++ 19.MultiThreading/src/threads/ThreadExampleThread.java
@@ -0,0 +1,36 @@
+package threads;
+
+class MyThread extends Thread
+{
+    public void run()
+    {
+        int i=1;
+        while(true)
+        {
+            System.out.println(i+"Hello");
+            i++;
+        }
+    }
+}
+
+public class ThreadExampleThread
+{
+    
+    
+    public static void main(String[] args) {
+        
+        
+        MyThread th=new MyThread();
+        
+        th.start();
+        
+        int i=1;
+        while(true)
+        {
+            System.out.println(i+"World");
+            i++;
+        }
+        
+    }
+    
+}
