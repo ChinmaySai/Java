82877a3da23df68d5485dff4c6f06b2c65daec48
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2024-11-15 16:04:17 +0530

    Commiting Data Type Section Changes

---
+++ 2.DataTypes/src/DataTypesConversion.java
@@ -0,0 +1,13 @@
+
+public class DataTypesConversion {
+
+	public static void main(String[] args) {
+
+		int x=5;
+		String binaryString=Integer.toBinaryString(x);
+		System.out.printf("%S\n",binaryString);
+		String s1=Integer.toOctalString(200);
+		System.out.println(s1);
+	}
+
+}
