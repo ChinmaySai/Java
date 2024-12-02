2cc876660f55f94c566c62e04041f7fd5cf0d187
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2024-12-02 08:36:42 +0530

    Commiting the New Changes of DataTypes

---
+++ 2.DataTypes/src/com/datatypes/DataTypeRange.java
@@ -0,0 +1,60 @@
+package com.datatypes;
+
+import java.lang.*;
+
+/**
+ * 
+ * main method prints the minimum,maximum and number of Bytes for  Primitive Data-types 
+ */
+
+public class DataTypeRange {
+
+	public static void main(String[] args) {
+		
+		//Primitive-Byte Wrapper--Byte
+        System.out.println("byte");
+        System.out.println("Byte.MIN_VALUE = "+Byte.MIN_VALUE);
+        System.out.println("Byte.MAX_VALUE = "+Byte.MAX_VALUE);
+        System.out.println("Byte.BYTES = "+Byte.BYTES);
+
+        //Primitive-short Wrapper--Short
+        System.out.println("\nshort");
+        System.out.println("Short.MIN_VALUE = "+Short.MIN_VALUE);
+        System.out.println("Short.MAX_VALUE = "+Short.MAX_VALUE);
+        System.out.println("Short.BYTES = "+Short.BYTES);
+
+		
+		//int --Primitive Wrapper--Integer
+		  System.out.println("\nint");
+		 System.out.println("Integer.MIN_VALUE = " + Integer.MIN_VALUE);
+	        System.out.println("Integer.MAX_VALUE = " + Integer.MAX_VALUE);
+	        System.out.println("Integer.Bytes = " + Integer.BYTES);
+
+	      
+	        //Primitive-long Wrapper--Long
+	        System.out.println("\nlong");
+	        System.out.println("Long.MIN_VALUE = "+Long.MIN_VALUE);
+	        System.out.println("Long.MAX_VALUE = "+Long.MAX_VALUE);
+	        System.out.println("Long.BYTES = "+Long.BYTES);
+
+	        //Primitive-float Wrapper--Float
+	        System.out.println("\nfloat");
+	        System.out.println("Float.MIN_VALUE = "+Float.MIN_VALUE);
+	        System.out.println("Float.MAX_VALUE = "+Float.MAX_VALUE);
+	        System.out.println("Float.BYTES = "+Float.BYTES);
+
+	        //Primitive-Double Wrapper--Double
+	        System.out.println("\ndouble");
+	        System.out.println("Double.MIN_VALUE = "+Double.MIN_VALUE);
+	        System.out.println("Double.MAX_VALUE = "+Double.MAX_VALUE);
+	        System.out.println("Double.BYTES = "+Double.BYTES);
+
+	        // //Primitive-char Wrapper--Character
+	        System.out.println("\nchar");
+	        System.out.println("Character.BYTES = "+Character.BYTES);
+  
+          
+
+	}
+
+}
---
+++ 2.DataTypes/src/com/datatypes/Varibale.java
@@ -0,0 +1,27 @@
+package com.datatypes;
+
+public class Varibale {
+
+	public static void main(String[] args) {
+
+		byte b=10;
+        short s=45;
+        int i=236;
+        long l=5L;
+        float f=5.6f;
+        double d=3.698; //3.96D //3.96d--Both are also valid
+        char ch='a';
+        boolean val=false;
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(ch);
+        System.out.println(val);
+		
+ 		
+	}
+
+}
