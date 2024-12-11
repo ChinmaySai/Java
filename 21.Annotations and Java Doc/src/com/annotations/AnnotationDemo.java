6c2fb8c888659dae8ccfa5b3a4af193947fc7f5f
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2024-12-11 21:52:58 +0530

    Committing Annotation Chnages

---
+++ 21.Annotations and Java Doc/src/com/annotations/AnnotationDemo.java
@@ -0,0 +1,26 @@
+package com.annotations;
+
+class Parent
+{
+	public void display()
+	{
+		
+	}
+}
+class Child extends Parent
+{
+	@Override
+	public void display()
+	{
+		
+	}
+}
+
+public class AnnotationDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
