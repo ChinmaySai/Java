09cf76cd89e1586bbb488a4aec2843f4bee9e13e
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2024-12-30 20:43:17 +0530

    Initial Commit of Date and Time API code changes

---
+++ 26.DateandTimeAPI/src/DateDemo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class DateDemo {
+
+	public static void main(String[] args) {
+	
+		System.out.println(System.currentTimeMillis());//Prints the Current time in MilliSeconds
+		System.out.println("Number of years "+System.currentTimeMillis()/1000/60/60/24/365);
+
+	}
+}
