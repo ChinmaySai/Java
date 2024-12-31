a48fb73c0f6d91d505998f0ef8549adc5db743de
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2024-12-31 23:58:36 +0530

    Committing the LocalDateTime Code changes.

---
+++ 26.DateandTimeAPI/src/com/jodatimeapi/LocalDateTimeExample.java
@@ -0,0 +1,15 @@
+package com.jodatimeapi;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class LocalDateTimeExample {
+
+	public static void main(String[] args) {
+
+
+		LocalDateTime lt=LocalDateTime.now();
+		System.out.println("Date and Time : "+lt);
+	}
+
+}
