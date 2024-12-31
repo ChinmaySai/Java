bea53d5a6cbe3720225681fb5990cd3b6c8586b7
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2024-12-31 23:40:37 +0530

    LocalDate Code Changes Commit.

--- 26.DateandTimeAPI/src/com/jodatimeapi/LocalDateExample.java
+++ 26.DateandTimeAPI/src/com/jodatimeapi/LocalDateExample.java
@@ -15,6 +15,18 @@ public class LocalDateExample {
        System.out.println("Date : "+ld1);
        LocalDate d2=LocalDate.now(ZoneId.of("Asia/Kolkata"));
        System.out.println(d2);
+       
+       LocalDate d4=LocalDate.of(2020,5,10);
+       System.out.println(d4);
+       
+       LocalDate d5=LocalDate.ofEpochDay(1000);
+       System.out.println("EPoch Date for 1000 days : "+d5);
+       
+       LocalDate d6=LocalDate.parse("2020-01-03");
+       System.out.println(d6);
+       
+       LocalDate d7=d6.plusMonths(5);
+       System.out.println("Date after adding Months: "+d7);
            
        
 	}
