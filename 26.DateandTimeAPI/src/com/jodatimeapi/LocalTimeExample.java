8894284559d7e5d39132a21420177d07c0aef826
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2024-12-31 23:54:35 +0530

    LocalTime Code changes Commit.

---
+++ 26.DateandTimeAPI/src/com/jodatimeapi/LocalTimeExample.java
@@ -0,0 +1,12 @@
+package com.jodatimeapi;
+
+import java.time.*;
+public class LocalTimeExample {
+
+	public static void main(String[] args) {
+
+		LocalTime lt=LocalTime.now();
+		System.out.println(lt);
+			}
+
+}
