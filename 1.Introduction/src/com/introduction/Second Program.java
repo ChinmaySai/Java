66998c99563da982db5fae821fa74bb926d5cb05
Chinmay Sai <41532698+ChinmaySai@users.noreply.github.com>
2025-01-08 21:30:34 +0530

    Second Program Code Changes

---
+++ 1.Introduction/src/com/introduction/Second Program.java
@@ -0,0 +1,10 @@
+package com.introduction;
+import java.io.*;
+
+public class SecondProgram {
+
+        public static void main(String args[])
+        {
+                System.out.println("Main Method Code changes Commit ");
+        }
+}
