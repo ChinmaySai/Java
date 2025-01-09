eaf90d3aca958f40ec1a5bf987b00e3bbcb4ab2a
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-01-09 22:57:10 +0530

    Exception Handling Example 2

---
+++ 18.ExceptionHandling/src/com/exceptionhandling/Example2.java
@@ -0,0 +1,29 @@
+package com.exceptionhandling;
+
+/**
+ * 
+ * 
+ * 
+ */
+public class Example2 {
+
+	public static void main(String[] args) {
+
+
+		
+		try
+		{
+
+			int a=20,b,c2=0;
+			int d=a/c2;
+		
+	}
+		catch(ArithmeticException e)
+		{
+		 System.out.println(e);
+		 
+		}
+		}
+;	
+
+}
