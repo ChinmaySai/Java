8ea6ffe6e0e38662e293897a330d2dde12e2f95b
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-01-09 22:37:59 +0530

    Exception Example 1 changes commit.

---
+++ 18.ExceptionHandling/src/com/exceptionhandling/Example1.java
@@ -0,0 +1,19 @@
+package com.exceptionhandling;
+
+/**
+ * 
+ * Java Program for Arithmetic Exception Example
+ */
+public class Example1 {
+
+	public static void main(String[] args) {
+
+		int a,b,c;
+		a=5;
+		b=0;
+		c=a/b;
+		
+		System.out.println(c);
+
+}
+}
