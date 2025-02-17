e1a53b553e5e0c73db883a72743aa82c64ea8054
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-02-17 09:04:07 +0530

    Updated Few Code Changes

--- 6.String Class Printing/src/com/string/StringPractice1.java
+++ 6.String Class Printing/src/com/string/StringPractice1.java
@@ -8,26 +8,26 @@ package com.string;
 	        String str=new String("Chinmay Sai  ");
 	        
 	        
-	        System.out.println(str.length());
-	        int len=str.length();
-	        System.out.println(len);
+	        //System.out.println(str.length());
+	       int len=str.length();
+	       System.out.println(len);
 	        
 	        
-	        String str2=str.toUpperCase();
+	        String str2=str.toUpperCase(); //Object will be Created in Heap
 	        System.out.println(str2);
 	        
 	        
 	        str=str.trim();
 	        System.out.println("After Trimming "+str);
 	        
-	        /*String str2=str.substring(3);
-	        System.out.println(str2);*/
+	        String str22=str.substring(3);
+	        System.out.println(str22);
+	        String str23=str.substring(1,3);
+	        System.out.println(str23);
 	        
 	        String str3=str.replace('e', 'M');
-	        //System.out.println(str2);
-	        
-	        System.out.println(str2+ " " +str);
-	        
+	        System.out.println(str3);
+	        	        
 	    }
 	    
 	}
