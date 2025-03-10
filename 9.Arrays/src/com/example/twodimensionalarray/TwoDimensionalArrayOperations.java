6f3d9a24ef2c5dd81d57ebbe5969900835de5f9e
Chinmay Sai <yanamalachinmaysai106@gmail.com>
2025-03-10 14:18:59 +0530

    Two Dimensional Array Practice Programs

---
+++ 9.Arrays/src/com/example/twodimensionalarray/TwoDimensionalArrayOperations.java
@@ -0,0 +1,21 @@
+package com.example.twodimensionalarray;
+import java.util.*;
+
+public class TwoDimensionalArrayOperations
+{
+    
+    
+    public static void main(String args[])
+    {
+        
+        String arr[]={"java","python","pascal","smalltalk","ada","basic"};
+        
+        Arrays.sort(arr);
+        
+        for(String x:arr)
+            System.out.println(x);
+        
+        
+        
+    }
+    }    
