0d91ca86f57237c0017466d660240bdb3f79d912
ChinmaySai <yanamalachinmaysai106@gmail.com>
2025-04-26 09:18:23 +0530

    Inner Class and Outer class Access Modifier

--- 17.Packages/src/com/demo/inner/Demo3.java
+++ 17.Packages/src/com/demo/inner/Demo3.java
@@ -1,5 +1,10 @@
 package com.demo.inner;
 
+//outer class modifier has to be public/default other wise it would result in compilation error.
 public class Demo3 {
 
+	class Inner
+	{
+		//Inner class modifier can be default/public/private/protected 
+	}
 }
