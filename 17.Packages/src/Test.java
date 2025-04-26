72aea8de56523e5e2fd54bec7e5a689194fb1198
ChinmaySai <yanamalachinmaysai106@gmail.com>
2025-04-26 08:55:52 +0530

    Package Changes Commit

--- 17.Packages/src/Test.java
+++ 17.Packages/src/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		//Directly referring to a class within the package
 		java.lang.String str1=new java.lang.String("Hello");
+		System.out.println(str1);
 	}
 }
---
+++ 17.Packages/src/mypackage/MyClassPackage.java
@@ -0,0 +1,10 @@
+package mypackage;
+import com.demo.inner.*;
+public class MyClassPackage
+{
+public static void main(String args[])
+{
+java.lang.String str="Chinmay";
+System.out.println(str);
+}
+}
